c7a9623f3517bf5ebac16994536b07974d183be6
25angel <viktor.raiko04@gmail.com>
2024-11-10 15:52:46 +0500

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Victor Raiko on 10.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
